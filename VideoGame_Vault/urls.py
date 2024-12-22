00a47deb0e7c4c5508fc4983ed3158c740855666
Jaime-Linares <jailinbar@alum.us.es>
2024-12-22 21:03:17 +0100

    [feat] base template and home view

--- VideoGame_Vault/urls.py
+++ VideoGame_Vault/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
 ]
 
 if settings.DEBUG:
--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from main.models import Developer, Genre, Plataform, Store, Video_game
 
-# Create your views here.
+
+
+# vista para mostrar la página de inicio
+def home(request):
+    developers = Developer.objects.all().count()
+    genres = Genre.objects.all().count()
+    plataforms = Plataform.objects.all().count()
+    stores = Store.objects.all().count()
+    video_games = Video_game.objects.all().count()
+    return render(request, 'home.html', {'developers':developers, 'genres':genres, 'plataforms':plataforms, 'stores':stores, 'video_games':video_games})
+
