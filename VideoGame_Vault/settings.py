92a10530f6bbabbd4006ea4b506a4ce9cf9d78da
Jaime-Linares <jailinbar@alum.us.es>
2024-12-22 18:44:56 +0100

    [feat] styles added to the app

--- VideoGame_Vault/settings.py
+++ VideoGame_Vault/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# staticfiles_dirs se define para poder usar los archivos estaticos en las plantillas
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
