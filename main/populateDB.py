e5f46ab932fca3c244a53bb8d642e016023660c0
Jaime-Linares <jailinbar@alum.us.es>
2024-12-23 21:50:58 +0100

    [feat] load data with web scraping, page Instant gaming (genre needs improvements). WIP

--- main/populateDB.py
+++ main/populateDB.py
@@ -1,13 +1,14 @@
 # encoding:utf-8
 from main.models import Developer, Genre, Plataform, Store, Video_game
 from bs4 import BeautifulSoup
-import os, re, ssl, lxml, urllib.request
+import os, ssl, urllib.request, locale
 from datetime import datetime
 # lineas para evitar error SSL
-from markdown_it.rules_inline import link
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
 getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
+
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 
 
 
@@ -19,10 +20,12 @@ def populate():
     Plataform.objects.all().delete()
     Store.objects.all().delete()
     Video_game.objects.all().delete()
+
+    NUM_PAGES = 3
     # cargamos los datos desde instant-gaming
-    populate_instant_gaming()
+    populate_instant_gaming(3)
     # cargamos los datos desde eneba
-    populate_eneba()
+    populate_eneba(3)
 
 
 # función auxiliar para tener los juegos en una lista y los generos de cada juego en un diccionario
@@ -49,13 +52,115 @@ def create_video_games(video_games_list, dic_genres, total_genres, store):
 
 
 # --- FUNCIONES INSTANT-GAMING ---------------------------------------------------------------------------------------------
-def populate_instant_gaming():
-    return None
+def populate_instant_gaming(num_pages):
+    video_games_list = []
+    dic_genres = {}
+    total_genres = []
+    dic_developers = {}
+    dic_plataforms = {}
+    store = Store.objects.create(name="Instant Gaming")
+    print("START INSTANT GAMING WEB SCRAPPING")
+
+    for pag in range(1, num_pages+1):
+        request = urllib.request.Request(f"https://www.instant-gaming.com/es/busquedas/?page={pag}", headers={'User-Agent': 'Mozilla/5.0'})
+        f = urllib.request.urlopen(request, timeout=3)
+        s = BeautifulSoup(f, 'lxml')
+        s_video_games = s.find("div", class_="listing-items").find_all("div", class_="force-badge")
+
+        for s_video_game in s_video_games:
+            name = str(list(s_video_game.find("div", class_="name").stripped_strings)[-1])
+            print(name)
+            url_inf = s_video_game.a['href'].strip()
+            print(url_inf)
+            url_img = s_video_game.find("img", class_="picture")['data-src'].strip()
+            print(url_img)
+
+            request2 = urllib.request.Request(url_inf, headers={'User-Agent': 'Mozilla/5.0'})
+            f2 = urllib.request.urlopen(request2)
+            s2 = BeautifulSoup(f2, 'lxml')
+
+            money = s2.find("div", class_="amount")
+            price = float(money.find("div", class_="total").string.strip().replace("€", "").strip()) if money.find("div", class_="total") else 0.0
+            print(price)
+            discount = int(money.find("div", class_="discounted").string.strip().replace("%", "").replace("-", "").strip()) if money.find("div", class_="discounted") else 0
+            print(discount)
+            print(s2.find("div", class_="ig-search-reviews-avg"))
+            s_score = s2.find("div", class_="ig-search-reviews-avg")
+            if s_score != None and s_score.string.strip() != "--":
+                score = float(s_score.string.strip())
+            else:
+                score = 0.0
+            print(score)
+            s_description = s2.find("div", class_="readable")
+            description = "".join(list(s_description.stripped_strings)) if s_description else ""
+            print(description)
+            release_date = datetime.strptime(s2.find("div", class_="release-date").get_text().split(" -")[0].strip(), "%d %B %Y") if s2.find("div", class_="release-date") else None
+            print(release_date)
+            developer, dic_developers = get_developer_instant_gaming(s2, dic_developers)
+            print(developer)
+            video_games_genres, total_genres = get_genres_instant_gaming(s2, total_genres)
+            print(video_games_genres)
+            plataform, dic_plataforms = get_plataform_instant_gaming(s2, dic_plataforms)
+            print(plataform)
+
+            video_games_list, dic_genres = create_video_game(name, url_inf, url_img, price, discount, score, description, release_date, developer, 
+                                                            video_games_genres, dic_genres, plataform, store, video_games_list)
+    
+    create_video_games(video_games_list, dic_genres, total_genres, store)
+    print("FINISH INSTANT GAMING WEB SCRAPPING")
+
+
+# función auxiliar para obtener el desarrollador de un videojuego
+def get_developer_instant_gaming(s2, dic_developers):
+    s_developer = s2.find("a", content="Developers")
+    if s_developer:
+        developer_string = s_developer.string.strip()
+    else:
+        developer_string = "Unknown"
+
+    if developer_string not in dic_developers:
+        developer = Developer.objects.create(name=developer_string)
+        dic_developers[developer_string] = developer
+    developer = dic_developers[developer_string]
+    return developer, dic_developers
+
+
+# función auxiliar para obtener los géneros de un videojuego
+def get_genres_instant_gaming(s2, total_genres):
+    s_genres = s2.find("div", class_="table-cell", string="Género:")
+    if s_genres:
+        s_genres = s_genres.next_sibling.next_sibling
+        genres = list(s_genres.find_all("a"))
+    else:
+        genres = []
+
+    video_games_genres = []
+    for a in genres:
+        genre = str(a.string.strip()).lower()
+        video_games_genres.append(genre)
+        if genre not in total_genres:
+            total_genres.append(genre)
+    return video_games_genres, total_genres
+
+
+# función auxiliar para obtener la plataforma de un videojuego
+def get_plataform_instant_gaming(s2, dic_plataforms):
+    s_plataform = s2.find("div", class_="subinfos").a
+    if s_plataform:
+        plataform_string = str(list(s_plataform.stripped_strings)[0]).lower()
+    else:
+        plataform_string = "Unknown"
+    
+    if plataform_string not in dic_plataforms:
+        plataform = Plataform.objects.create(name=plataform_string)
+        dic_plataforms[plataform_string] = plataform
+    plataform = dic_plataforms[plataform_string]
+    return plataform, dic_plataforms
 
 
 
 # --- FUNCIONES ENEBA ------------------------------------------------------------------------------------------------------
-def populate_eneba():
+def populate_eneba(num_pages):
     return None
 
 
